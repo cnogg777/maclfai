22f917953bd210e00c3684710fccd3dbd8c8333c
Maxkrator1 <max.villela@duocuc.cl>
2026-05-04 14:04:47 -0400

    Delete src directory

--- src/app/modules/estudio/practica/practica.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-practica',
-  imports: [],
-  templateUrl: './practica.html',
-  styleUrl: './practica.css',
-})
-export class Practica {}
--- src/app/modules/simulacion/simulacion/simulacion.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-simulacion',
-  imports: [],
-  templateUrl: './simulacion.html',
-  styleUrl: './simulacion.css',
-})
-export class Simulacion {}
